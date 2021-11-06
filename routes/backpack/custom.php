0d8f2c4d691b36a76c937c5b65ec5cff9a390304
Shift <shift@laravelshift.com>
2021-11-06 05:49:14 +0000

    Convert options array to fluent methods
    Laravel 8 adopts the tuple syntax for controller actions. Since the old options array is incompatible with this syntax, Shift converted them to use modern, fluent methods.

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,14 +6,11 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
-Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))->namespace('App\Http\Controllers\Admin')->group([
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
         (array) config('backpack.base.middleware_key', 'admin')
-    ),
-    'namespace'  => 'App\Http\Controllers\Admin',
-], function () { // custom admin routes
+    ),], function () { // custom admin routes
     Route::crud('statistics', 'StatisticsCrudController');
     Route::crud('user', 'UserCrudController');
     Route::crud('admin-activity', 'AdminActivityCrudController');
